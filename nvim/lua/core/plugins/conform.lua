d6920a1f0c25ec6ca1abcae1356f0340d999eca3
PA Parent <paparent@paparent.me>
2024-07-28 16:04:00 -0400

    Add Conform and setup stylua

---
+++ nvim/lua/core/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    ---@module "conform"
+    ---@type conform.setupOpts
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+        },
+        default_format_opts = { lsp_format = "last" },
+        format_on_save = { timeout_ms = 500 },
+    },
+}
